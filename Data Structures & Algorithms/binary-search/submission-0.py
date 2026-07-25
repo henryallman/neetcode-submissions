c5817631f285c865833501037194cfc16e285eb8
Henry Allman <henryallman@gmail.com>
2026-07-24 19:07:20 -0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for num in nums:
+            if num == target:
+                return nums.index(num)
+        return -1
